543a6e8cbc30273269af8fa75eae36f8ce16cc65
chchen59 <chchen59.nvt@gmail.com>
2025-05-02 16:51:28 +0800

    Update README.md

--- M55M1BSP-3.01.001/SampleCode/NuEdgeWise/_NN_ExecuTorch/README.md
+++ M55M1BSP-3.01.001/SampleCode/NuEdgeWise/_NN_ExecuTorch/README.md
@@ -8,7 +8,47 @@ A template sample code for executorch Arm backend
 2. Copy .pte file to SD card and rename to model.pte
 3. Insert SD card to NUMAKER-M55M1 board
 4. Run
+## Troubleshooting
+1. If got "Memory allocation filed" message, enlarge method_allocation_pool space.
 
+![memory allocate fail](Picture/memory_allocate_fail.png) 
+```
+__attribute__((section(".bss.sram.data"), aligned(32)))
+uint8_t method_allocation_pool[1400 * 1024U];
+```
+2. If got "Missing operator" message, search operator code from "$BSP\ThirdParty\executorch\_prebuilt\kernels\portable\RegisterCodegenUnboxedKernelsEverything.cpp" or "$BSP\ThirdParty\executorch\_prebuilt\kernels\quantized\RegisterCodegenUnboxedKernelsEverything.cpp" and copy kernel operator code to "RegisterNativeKernels.cpp".
 
+![miss operator](Picture/miss_operator.png)
 
+```
+    Kernel(
+        "quantized_decomposed::dequantize_per_tensor.out",
+        [](torch::executor::KernelRuntimeContext & context, EValue** stack) {
+            EValue& input = *stack[0];
+			EValue& scale = *stack[1];
+			EValue& zero_point = *stack[2];
+			EValue& quant_min = *stack[3];
+			EValue& quant_max = *stack[4];
+			EValue& dtype = *stack[5];
+			EValue& out_dtype = *stack[6];
+			EValue& out = *stack[7];
+			const torch::executor::Tensor & input_base = input.to<torch::executor::Tensor>();
+			double scale_base = scale.to<double>();
+			int64_t zero_point_base = zero_point.to<int64_t>();
+			int64_t quant_min_base = quant_min.to<int64_t>();
+			int64_t quant_max_base = quant_max.to<int64_t>();
+			torch::executor::ScalarType dtype_base = dtype.to<torch::executor::ScalarType>();
+			
+			auto out_dtype_opt_out = out_dtype.toOptional<torch::executor::ScalarType>();
+						
+			torch::executor::Tensor & out_base = out.to<torch::executor::Tensor>();
+        
+            internal::EventTracerProfileOpScope event_tracer_op_scope(context.internal_event_tracer(), "native_call_dequantize_per_tensor.out");
+            EXECUTORCH_SCOPE_PROF("native_call_dequantize_per_tensor.out");
+            torch::executor::native::dequantize_per_tensor_out(context, input_base, scale_base, zero_point_base, quant_min_base, quant_max_base, dtype_base, out_dtype_opt_out, out_base);
+            internal::event_tracer_log_evalue(context.internal_event_tracer(), *stack[7]);    
+
+        }
+    ),
+```
 [^1]:[Building and Running ExecuTorch with ARM Backend](https://pytorch.org/executorch/stable/executorch-arm-delegate-tutorial.html)
--- M55M1BSP-3.01.001/SampleCode/NuEdgeWise/_NN_ExecuTorch/RegisterNativeKernels.cpp
+++ M55M1BSP-3.01.001/SampleCode/NuEdgeWise/_NN_ExecuTorch/RegisterNativeKernels.cpp
@@ -14,7 +14,7 @@
 #include "Portable/NativeFunctions.h" // Generated Function import headers
 #include "Quantized/NativeFunctions.h" // Generated Function import headers
 
-//Copy CPU native kernel operator from  RegisterCodegenUnboxedKernelsEverything.cpp to here. These require by NN model
+//Copy CPU native kernel operator from RegisterCodegenUnboxedKernelsEverything.cpp to here. These require by NN model
 
 //using KernelArrayRef = ::torch::executor::ArrayRef<::torch::executor::Kernel>;
 using KernelSpan = ::executorch::runtime::Span<
@@ -27,37 +27,29 @@ static Kernel kernels_to_register[] = {
         "aten::max_pool2d_with_indices.out",
         [](torch::executor::KernelRuntimeContext & context, EValue** stack) {
             EValue& self = *stack[0];
-    	EValue& kernel_size = *stack[1];
-    	EValue& stride = *stack[2];
-    	EValue& padding = *stack[3];
-    	EValue& dilation = *stack[4];
-    	EValue& ceil_mode = *stack[5];
-    	EValue& out = *stack[6];
-    	EValue& indices = *stack[7];
-    	const torch::executor::Tensor & self_base = self.to<torch::executor::Tensor>();
+			EValue& kernel_size = *stack[1];
+			EValue& stride = *stack[2];
+			EValue& padding = *stack[3];
+			EValue& dilation = *stack[4];
+			EValue& ceil_mode = *stack[5];
+			EValue& out = *stack[6];
+			EValue& indices = *stack[7];
+			const torch::executor::Tensor & self_base = self.to<torch::executor::Tensor>();
     	
     	    torch::executor::ArrayRef<int64_t> kernel_size_list_out = kernel_size.toIntList();
-    	                
-    	
     	    torch::executor::ArrayRef<int64_t> stride_list_out = stride.toIntList();
-    	                
-    	
     	    torch::executor::ArrayRef<int64_t> padding_list_out = padding.toIntList();
-    	                
-    	
     	    torch::executor::ArrayRef<int64_t> dilation_list_out = dilation.toIntList();
-    	                
-    	bool ceil_mode_base = ceil_mode.to<bool>();
-    	torch::executor::Tensor & out_base = out.to<torch::executor::Tensor>();
-    	torch::executor::Tensor & indices_base = indices.to<torch::executor::Tensor>();
-    
+
+			bool ceil_mode_base = ceil_mode.to<bool>();
+			torch::executor::Tensor & out_base = out.to<torch::executor::Tensor>();
+			torch::executor::Tensor & indices_base = indices.to<torch::executor::Tensor>();    
             internal::EventTracerProfileScope event_tracer_scope(context.internal_event_tracer(), "native_call_max_pool2d_with_indices.out");
             EXECUTORCH_SCOPE_PROF("native_call_max_pool2d_with_indices.out");
             torch::executor::native::max_pool2d_with_indices_out(context, self_base, kernel_size_list_out, stride_list_out, padding_list_out, dilation_list_out, ceil_mode_base, out_base, indices_base);
             internal::event_tracer_log_evalue(context.internal_event_tracer(), *stack[6]);
-    internal::event_tracer_log_evalue(context.internal_event_tracer(), *stack[7]);
-    
-            
+			internal::event_tracer_log_evalue(context.internal_event_tracer(), *stack[7]);
+
         }
     ),
 
@@ -65,19 +57,18 @@ static Kernel kernels_to_register[] = {
         "aten::_log_softmax.out",
         [](torch::executor::KernelRuntimeContext & context, EValue** stack) {
             EValue& self = *stack[0];
-    	EValue& dim = *stack[1];
-    	EValue& half_to_float = *stack[2];
-    	EValue& out = *stack[3];
-    	const torch::executor::Tensor & self_base = self.to<torch::executor::Tensor>();
-    	int64_t dim_base = dim.to<int64_t>();
-    	bool half_to_float_base = half_to_float.to<bool>();
-    	torch::executor::Tensor & out_base = out.to<torch::executor::Tensor>();
+			EValue& dim = *stack[1];
+			EValue& half_to_float = *stack[2];
+			EValue& out = *stack[3];
+			const torch::executor::Tensor & self_base = self.to<torch::executor::Tensor>();
+			int64_t dim_base = dim.to<int64_t>();
+			bool half_to_float_base = half_to_float.to<bool>();
+			torch::executor::Tensor & out_base = out.to<torch::executor::Tensor>();
     
             internal::EventTracerProfileScope event_tracer_scope(context.internal_event_tracer(), "native_call__log_softmax.out");
             EXECUTORCH_SCOPE_PROF("native_call__log_softmax.out");
             torch::executor::native::log_softmax_out(context, self_base, dim_base, half_to_float_base, out_base);
             internal::event_tracer_log_evalue(context.internal_event_tracer(), *stack[3]);
-    
             
         }
     ),
@@ -86,64 +77,56 @@ static Kernel kernels_to_register[] = {
         "quantized_decomposed::quantize_per_tensor.out",
         [](torch::executor::KernelRuntimeContext & context, EValue** stack) {
             EValue& input = *stack[0];
-    	EValue& scale = *stack[1];
-    	EValue& zero_point = *stack[2];
-    	EValue& quant_min = *stack[3];
-    	EValue& quant_max = *stack[4];
-    	EValue& dtype = *stack[5];
-    	EValue& out = *stack[6];
-    	const torch::executor::Tensor & input_base = input.to<torch::executor::Tensor>();
-    	double scale_base = scale.to<double>();
-    	int64_t zero_point_base = zero_point.to<int64_t>();
-    	int64_t quant_min_base = quant_min.to<int64_t>();
-    	int64_t quant_max_base = quant_max.to<int64_t>();
-    	torch::executor::ScalarType dtype_base = dtype.to<torch::executor::ScalarType>();
-    	torch::executor::Tensor & out_base = out.to<torch::executor::Tensor>();
-    
-    
+			EValue& scale = *stack[1];
+			EValue& zero_point = *stack[2];
+			EValue& quant_min = *stack[3];
+			EValue& quant_max = *stack[4];
+			EValue& dtype = *stack[5];
+			EValue& out = *stack[6];
+			const torch::executor::Tensor & input_base = input.to<torch::executor::Tensor>();
+			double scale_base = scale.to<double>();
+			int64_t zero_point_base = zero_point.to<int64_t>();
+			int64_t quant_min_base = quant_min.to<int64_t>();
+			int64_t quant_max_base = quant_max.to<int64_t>();
+			torch::executor::ScalarType dtype_base = dtype.to<torch::executor::ScalarType>();
+			torch::executor::Tensor & out_base = out.to<torch::executor::Tensor>();
+        
             internal::EventTracerProfileOpScope event_tracer_op_scope(context.internal_event_tracer(), "native_call_quantize_per_tensor.out");
             EXECUTORCH_SCOPE_PROF("native_call_quantize_per_tensor.out");
             torch::executor::native::quantize_per_tensor_out(context, input_base, scale_base, zero_point_base, quant_min_base, quant_max_base, dtype_base, out_base);
             internal::event_tracer_log_evalue(context.internal_event_tracer(), *stack[6]);
     
-            
-    
         }
     ),
-
     Kernel(
         "quantized_decomposed::dequantize_per_tensor.out",
         [](torch::executor::KernelRuntimeContext & context, EValue** stack) {
             EValue& input = *stack[0];
-    	EValue& scale = *stack[1];
-    	EValue& zero_point = *stack[2];
-    	EValue& quant_min = *stack[3];
-    	EValue& quant_max = *stack[4];
-    	EValue& dtype = *stack[5];
-    	EValue& out_dtype = *stack[6];
-    	EValue& out = *stack[7];
-    	const torch::executor::Tensor & input_base = input.to<torch::executor::Tensor>();
-    	double scale_base = scale.to<double>();
-    	int64_t zero_point_base = zero_point.to<int64_t>();
-    	int64_t quant_min_base = quant_min.to<int64_t>();
-    	int64_t quant_max_base = quant_max.to<int64_t>();
-    	torch::executor::ScalarType dtype_base = dtype.to<torch::executor::ScalarType>();
-    	
-    	    auto out_dtype_opt_out = out_dtype.toOptional<torch::executor::ScalarType>();
-    	            
-    	torch::executor::Tensor & out_base = out.to<torch::executor::Tensor>();
-    
-    
+			EValue& scale = *stack[1];
+			EValue& zero_point = *stack[2];
+			EValue& quant_min = *stack[3];
+			EValue& quant_max = *stack[4];
+			EValue& dtype = *stack[5];
+			EValue& out_dtype = *stack[6];
+			EValue& out = *stack[7];
+			const torch::executor::Tensor & input_base = input.to<torch::executor::Tensor>();
+			double scale_base = scale.to<double>();
+			int64_t zero_point_base = zero_point.to<int64_t>();
+			int64_t quant_min_base = quant_min.to<int64_t>();
+			int64_t quant_max_base = quant_max.to<int64_t>();
+			torch::executor::ScalarType dtype_base = dtype.to<torch::executor::ScalarType>();
+			
+			auto out_dtype_opt_out = out_dtype.toOptional<torch::executor::ScalarType>();
+						
+			torch::executor::Tensor & out_base = out.to<torch::executor::Tensor>();
+        
             internal::EventTracerProfileOpScope event_tracer_op_scope(context.internal_event_tracer(), "native_call_dequantize_per_tensor.out");
             EXECUTORCH_SCOPE_PROF("native_call_dequantize_per_tensor.out");
             torch::executor::native::dequantize_per_tensor_out(context, input_base, scale_base, zero_point_base, quant_min_base, quant_max_base, dtype_base, out_dtype_opt_out, out_base);
-            internal::event_tracer_log_evalue(context.internal_event_tracer(), *stack[7]);
-    
-            
-    
+            internal::event_tracer_log_evalue(context.internal_event_tracer(), *stack[7]);    
+
         }
     ),
-		
 };
 
 // Explicitly convert to ArrayRef, so that the API can take an empty C array of
